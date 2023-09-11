88ec3af82be4735f9a8f771a9b224d8ba87f269f
Ellie Lambert <ebezjian@gmail.com>
2023-09-11 05:46:49 -0400

    added file saves

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import *as SplashScreen from "expo-splash-screen";
 import { useCallback } from 'react';
+import BottomTabNavigation from './navigation/BottomTabNavigation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,10 +28,15 @@ export default function App() {
     return null; }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>You're the best coder!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Bottom Navigation'
+          component={BottomTabNavigation}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
